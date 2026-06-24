99d09ce4d5405bbf72c9d02e64bac443d6bb231c
Макеева Татьяна <tyh94@mail.ru>
2026-06-24 10:39:23 +0700

    Update storage

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["InAppPurchase"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/tyh94/Storage.git", from: "1.0.2"),
+        .package(url: "https://github.com/tyh94/Storage.git", from: "1.0.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
